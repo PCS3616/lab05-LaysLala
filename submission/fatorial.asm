678716d623dd77af8e9a1e787d16b5b182e0736b
Lays <160974852+LaysLala@users.noreply.github.com>
2025-02-16 19:26:10 -0300

    Add files via upload

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,41 +1,30 @@
-LD N
-JZ /032
-SC /040
-SC /010
-HM /060
+        LD  N        ; Carrega N
+        JZ  FIM      
+        SC  FATORIAL      
+        SC  SAIDA    
+        HM          
 
-JP /000
-LD /100
-MM /106
-LD /100
-SB /104
-MM /100
-JZ /050
-LD /106
-ML /100
-MM /106
-JP /016
+FIM     JP  /000     
 
-@ /0032
-LD /104
-MM /102
-HM /016
+FATORIAL     LD  N        
+        MM  AUX     
+        LD  N        
+        SB  UM       
+        MM  N        
+        JZ  BASE     
+        LD  AUX     
+        ML  N        
+        MM  AUX    
+        JP  FATORIAL 
 
-@ /0040
-JP /000
-LD /100
-MM /108
-LD /108
-SB /104
+BASE    LD  AUX     
+        MM  RESULTADO      
+        RS  FATORIAL      
 
-JZ /032
-RS /040
-LD /106
-MM RES
-RS /010
+SAIDA   JP  /000     
 
 @ /0100
-N       K  /0005    ; Valor de entrada para calcular fatorial (N = 5)
-RES     K  /0000    ; Armazena o resultado do fatorial
-UM      K  /0001    ; Constante 1 para decremento
-TEMP    K  /0000    ; Variável temporária
+N            K  /0005    
+RESULTAO     K  /0000   
+UM           K  /0001   
+AUX          K  /0000    
